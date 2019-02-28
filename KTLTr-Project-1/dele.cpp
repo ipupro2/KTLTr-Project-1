10e20f8ca5c60802046af4f54bfa107078112b61
thuy13705 <48061317+thuy13705@users.noreply.github.com>
2019-03-01 01:36:17 +0700

    Add files via upload

---
+++ KTLTr-Project-1/dele.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void Delete(int a[],int &n,int vt)
+{
+	int i;
+	    for (int i=vt; i<n-1; i++)
+	        a[i]=a[i+1];
+	    n--;
+}
+void Deletearray(int a[],int &m,int vt[],int n)
+{
+	int i, j;
+	for (int i=0; i<n; i++)
+	    a[vt[i]]=0;
+	for (int i=0; i<m; i++)
+	   if (a[i]==0)
+	    Delete(a,m,i);
+}
+
+
+
+
+
