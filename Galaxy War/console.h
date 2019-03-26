d7ed03d02bc5798029d74fe290d786a7f7df8da3
Võ Minh Triều <minhtrieuvo600@gmail.com>
2019-03-26 23:17:58 +0700

    Delete old files

--- Galaxy War/console.h
+++
@@ -1,8 +0,0 @@
-﻿#include <Windows.h>
-
-//Hide blinking cursor in console
-//Ẩn con trỏ nhấp nháy trong màn hình console
-void HideCursor();
-//Move cursor to specified position
-//Di chuyển con trỏ đến vị trí chỉ định (r: dòng, c: cột)
-void GotoPosition(int r, int c);
